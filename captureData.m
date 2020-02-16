02fde2b4645b10f7c9e5829d591f62fbaac37afd
Angus Fotherby <angusfotherby@gmail.com>
2020-02-16 13:22:20 +0000

    Record RMS difference between target profile and current profile, rather than RMS difference between current and previous profiles.

--- captureData.m
+++ captureData.m
@@ -7,4 +7,4 @@ record_18 = horzcat(record_18, SO4_18);
 record_CH4 = horzcat(record_CH4, CH4);
 record_SO4 = horzcat(record_SO4, SO4);
 record_i = horzcat(record_i, i);
-record_RMS = horzcat(record_RMS, RMSSO4);
+record_RMS = horzcat(record_RMS, targetRMS);
