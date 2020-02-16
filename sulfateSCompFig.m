d05203de4344c817d9149ccb5814078cf22de5ba
Angus Fotherby <angusfotherby@gmail.com>
2020-02-16 13:44:38 +0000

    Changed file structure for loading data files and some changes to the plot formatting.

--- sulfateSCompFig.m
+++ sulfateSCompFig.m
@@ -1,9 +1,10 @@
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case1.mat')
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case1.mat')
+
 depth = 1:l;
 depth = depth .* sqrt(2);
 depth = depth';
-labels = labelGen(record_32, record_33, record_34);
 tiledlayout(4,2);
+
 nexttile();
 %% Chemical concentration (left most graph).
 % Plot data.
@@ -11,17 +12,20 @@ plotOutput.depthPlot(record_SO4, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse', 'YLim',[0 l * dz],'XLim',[0 27],'XAxisLocation','Top');
 xlabel(gca, '[SO_4] / mM');
-ylabel(gca, 'Depth / cm');
+ylabel(gca, {'Case 1' 'Depth / cm'})
 title(gca, 'Sulfate concentration');
+legend('off')
+
 nexttile();
 plotOutput.depthPlot(outputd34, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse','XLim',[20,100],'YLim',[0 l * dz], 'XAxisLocation', 'Top');
 xlabel(gca, '\delta^{34}S');
 ylabel(gca, 'Depth / cm');
-title(gca, 'Sulfure isotope composition');
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case2.mat');
-labels = labelGen(record_32, record_33, record_34);
+title(gca, 'Sulfur isotope composition');
+
+
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case2.mat');
 nexttile();
 %% Chemical concentration (left most graph).
 % Plot data.
@@ -29,17 +33,19 @@ plotOutput.depthPlot(record_SO4, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse', 'YLim',[0 l * dz],'XLim',[0 27],'XAxisLocation','Top');
 xlabel(gca, '[SO_4] / mM');
-ylabel(gca, 'Depth / cm');
-title(gca, 'Sulfate concentration');
+ylabel(gca, {'Case 2' 'Depth / cm'})
+legend('off')
+
+
 nexttile();
 plotOutput.depthPlot(outputd34, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse','XLim',[20,100],'YLim',[0 l * dz], 'XAxisLocation', 'Top');
 xlabel(gca, '\delta^{34}S');
 ylabel(gca, 'Depth / cm');
-title(gca, 'Sulfure isotope composition');
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case3.mat');
-labels = labelGen(record_32, record_33, record_34);
+
+
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case3.mat');
 nexttile();
 %% Chemical concentration (left most graph).
 % Plot data.
@@ -47,16 +53,19 @@ plotOutput.depthPlot(record_SO4, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse', 'YLim',[0 l * dz],'XLim',[0 27],'XAxisLocation','Top');
 xlabel(gca, '[SO_4] / mM');
-ylabel(gca, 'Depth / cm');
-title(gca, 'Sulfate concentration');
+ylabel(gca, {'Case 3' 'Depth / cm'})
+legend('off')
+
+
 nexttile();
 plotOutput.depthPlot(outputd34, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse','XLim',[20,100],'YLim',[0 l * dz], 'XAxisLocation', 'Top');
 xlabel(gca, '\delta^{34}S');
 ylabel(gca, 'Depth / cm');
-title(gca, 'Sulfure isotope composition');
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case4.mat');
+
+
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case4.mat');
 nexttile();
 %% Chemical concentration (left most graph).
 % Plot data.
@@ -64,13 +73,14 @@ plotOutput.depthPlot(record_SO4, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse', 'YLim',[0 l * dz],'XLim',[0 27],'XAxisLocation','Top');
 xlabel(gca, '[SO_4] / mM');
-ylabel(gca, 'Depth / cm');
-title(gca, 'Sulfate concentration');
+ylabel(gca, {'Case 4' 'Depth / cm'})
+legend('off')
+
 % Select axis and plot data.
+
 nexttile();
 plotOutput.depthPlot(outputd34, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse','XLim',[20,100],'YLim',[0 l * dz], 'XAxisLocation', 'Top');
 xlabel(gca, '\delta^{34}S');
 ylabel(gca, 'Depth / cm');
-title(gca, 'Sulfure isotope composition');
