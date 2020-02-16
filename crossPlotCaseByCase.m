58f0f404c10cc503267e76195e8b38653d9c47db
Angus Fotherby <angusfotherby@gmail.com>
2020-02-16 13:38:10 +0000

    Changed file structure for loading files and start using percentage progress labels instead of consecutive profile RMS labels.

--- crossPlotCaseByCase.m
+++ crossPlotCaseByCase.m
@@ -1,79 +1,81 @@
-tiledlayout(4,2);
+t = tiledlayout(4,2);
 
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case1.mat')
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case1.mat')
 
 depth = 1:l;
 depth = depth .* sqrt(2);
 depth = depth';
 
 nexttile()
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case1.mat')
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case1.mat')
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit); 
 plotOutput.crossPlot(outputd34, outputd18, labels);
 set(gca, 'YLim', [8 22], 'XLim', [20 80]);
 xlabel(gca, '\delta^{34}S')
-ylabel(gca, '\delta^{18}O')
-title(gca, 'Cross-plot of isotope composition')
+ylabel(gca, {'Case 1' '\delta^{18}O'})
+title('Recycling = 0')
+legend('off')
 
 nexttile()
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=10/case1.mat')
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case1.mat')
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit); 
 plotOutput.crossPlot(outputd34, outputd18, labels);
 set(gca, 'YLim', [8 22], 'XLim', [20 80]);
 xlabel(gca, '\delta^{34}S')
 ylabel(gca, '\delta^{18}O')
-title(gca, 'Cross-plot of isotope composition')
+title('Recyling = 10')
 
 nexttile()
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case2.mat')
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case2.mat')
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit); 
 plotOutput.crossPlot(outputd34, outputd18, labels);
 set(gca, 'YLim', [8 22], 'XLim', [20 80]);
 xlabel(gca, '\delta^{34}S')
-ylabel(gca, '\delta^{18}O')
-title(gca, 'Cross-plot of isotope composition')
+ylabel(gca, {'Case 2' '\delta^{18}O'})
+legend('off')
+
 
 nexttile()
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=10/case2.mat')
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case2.mat')
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit); 
 plotOutput.crossPlot(outputd34, outputd18, labels);
 set(gca, 'YLim', [8 22], 'XLim', [20 80]);
 xlabel(gca, '\delta^{34}S')
 ylabel(gca, '\delta^{18}O')
-title(gca, 'Cross-plot of isotope composition')
+
 
 nexttile()
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case3.mat')
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case3.mat')
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit); 
 plotOutput.crossPlot(outputd34, outputd18, labels);
 set(gca, 'YLim', [8 22], 'XLim', [20 80]);
 xlabel(gca, '\delta^{34}S')
-ylabel(gca, '\delta^{18}O')
-title(gca, 'Cross-plot of isotope composition')
+ylabel(gca, {'Case 3' '\delta^{18}O'})
+legend('off')
+
 
 nexttile()
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=10/case3.mat')
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case3.mat')
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit); 
 plotOutput.crossPlot(outputd34, outputd18, labels);
 set(gca, 'YLim', [8 22], 'XLim', [20 80]);
 xlabel(gca, '\delta^{34}S')
 ylabel(gca, '\delta^{18}O')
-title(gca, 'Cross-plot of isotope composition')
 
 nexttile()
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case4.mat')
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case4.mat')
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit); 
 plotOutput.crossPlot(outputd34, outputd18, labels);
 set(gca, 'YLim', [8 22], 'XLim', [20 80]);
 xlabel(gca, '\delta^{34}S')
-ylabel(gca, '\delta^{18}O')
-title(gca, 'Cross-plot of isotope composition')
+ylabel(gca, {'Case 4' '\delta^{18}O'})
+legend('off')
+
 
 nexttile()
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=10/case4.mat')
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case4.mat')
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit); 
 plotOutput.crossPlot(outputd34, outputd18, labels);
 set(gca, 'YLim', [8 22], 'XLim', [20 80]);
 xlabel(gca, '\delta^{34}S')
 ylabel(gca, '\delta^{18}O')
-title(gca, 'Cross-plot of isotope composition')
--- oxygenIsotopeDepthFig.m
+++ oxygenIsotopeDepthFig.m
@@ -1,89 +1,84 @@
 tiledlayout(4,2);
 
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case1.mat')
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case1.mat')
 
 depth = 1:l;
 depth = depth .* sqrt(2);
 depth = depth';
 
 nexttile()
-labels = labelGen(record_32, record_33, record_34);
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit);
 plotOutput.depthPlot(outputd18, depth, labels);
-% Format graph.
 set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
-xlabel(gca, '\delta^{18}O')
-ylabel(gca, 'Depth / cm')
-title(gca, 'Oxygen isotope composition')
+xlabel(gca, '\delta^{18}O');
+ylabel(gca, {'Case 1' 'Depth / cm'});
+title(gca, 'Recycling = 0');
+legend('off')
 
 nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=10/case1.mat');
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case1.mat');
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit);
 plotOutput.depthPlot(outputd18, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
 xlabel(gca, '\delta^{18}O');
 ylabel(gca, 'Depth / cm');
-title(gca, 'Oxygen isotope composition');
+title(gca, 'Recycling = 10');
 
 nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case2.mat');
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case2.mat');
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit);
+plotOutput.depthPlot(outputd18, depth, labels);
+% Format graph.
+set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
+xlabel(gca, '\delta^{18}O');
+ylabel(gca, {'Case 2' 'Depth / cm'})
+legend('off')
+
+nexttile();
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case2.mat');
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit)
 plotOutput.depthPlot(outputd18, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
 xlabel(gca, '\delta^{18}O');
 ylabel(gca, 'Depth / cm');
-title(gca, 'Oxygen isotope composition');
 
 nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=10/case2.mat');
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case3.mat');
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit)
+plotOutput.depthPlot(outputd18, depth, labels);
+% Format graph.
+set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
+xlabel(gca, '\delta^{18}O');
+ylabel(gca, {'Case 3' 'Depth / cm'})
+legend('off')
+
+nexttile();
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case3.mat');
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit)
 plotOutput.depthPlot(outputd18, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
 xlabel(gca, '\delta^{18}O');
 ylabel(gca, 'Depth / cm');
-title(gca, 'Oxygen isotope composition');
+
 
 nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case3.mat');
-labels = labelGen(record_32, record_33, record_34);
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case4.mat');
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit)
+plotOutput.depthPlot(outputd18, depth, labels);
+% Format graph.
+set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
+xlabel(gca, '\delta^{18}O');
+ylabel(gca, {'Case 4' 'Depth / cm'})
+legend('off')
+
+nexttile();
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case4.mat');
+labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit)
 plotOutput.depthPlot(outputd18, depth, labels);
 % Format graph.
 set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
 xlabel(gca, '\delta^{18}O');
 ylabel(gca, 'Depth / cm');
-title(gca, 'Oxygen isotope composition');
-
-nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=10/case3.mat');
-labels = labelGen(record_32, record_33, record_34);
-plotOutput.depthPlot(outputd18, depth, labels);
-% Format graph.
-set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
-xlabel(gca, '\delta^{18}O');
-ylabel(gca, 'Depth / cm');
-title(gca, 'Oxygen isotope composition');
-
-
-nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=0/case4.mat');
-labels = labelGen(record_32, record_33, record_34);
-plotOutput.depthPlot(outputd18, depth, labels);
-% Format graph.
-set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
-xlabel(gca, '\delta^{18}O');
-ylabel(gca, 'Depth / cm');
-title(gca, 'Oxygen isotope composition');
-
-nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/rec=10/case4.mat');
-labels = labelGen(record_32, record_33, record_34);
-plotOutput.depthPlot(outputd18, depth, labels);
-% Format graph.
-set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
-xlabel(gca, '\delta^{18}O');
-ylabel(gca, 'Depth / cm');
-title(gca, 'Oxygen isotope composition');
-
-
