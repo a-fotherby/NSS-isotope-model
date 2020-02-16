26432215dee1f45e559fb82f3aed9563af08a5a4
Angus Fotherby <angusfotherby@gmail.com>
2020-02-16 13:26:44 +0000

    Added a second function to generate labels for percentage progress to the final profile.

--- labelGen.m
+++ labelGen.m
@@ -1,5 +1,13 @@
-function [labels] = labelGen(x, y, z)
-    total = x + y + z;
-    rms = rmsCalc.calculate(total);
-    labels = rmsCalc.normalise(rms);
+classdef labelGen
+    methods(Static)
+        function [labels] = rmsPercentageNormalised(x, y, z)
+        total = x + y + z;
+        rms = rmsCalc.calculate(total);
+        labels = rmsCalc.normalise(rms);
+        end
+
+        function [labels] = percentageProgress(initialValue, currentValue, targetValue)
+            labels = (initialValue - currentValue) ./ (initialValue - targetValue) * 100;
+	end
+    end
 end
