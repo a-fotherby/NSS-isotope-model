dda3f4ee4a0672823010707fdde2bf00333c537d
Angus Fotherby <angusfotherby@gmail.com>
2020-02-16 13:49:19 +0000

    Change logic for deciding when to record profiles to stop erroneous behaviour when consecutive profiles are making large jumps in percentage progress (>5%  per time step) e.g. in case 2.

--- recordProfileRMS.m
+++ recordProfileRMS.m
@@ -2,15 +2,24 @@
 % Compare floor of pProgess to intervals of 5 to capture at 5% increments to steady state.
 % Use a flag to ensure that each percentage increment is only recorded once.
 
-if flag == 0
-    if floor(pProgress) == pProgressNext
-        captureData;
+%if flag == 0
+%    if floor(pProgress) >= pProgressNext
+%        captureData;
+%
+%        pProgressNext = pProgressNext + 5;
+%        flag = 1;
+%    end
+%else
+%    if floor(pProgress) >= pProgressNext - 4
+%        flag = 0;
+%    end
+%end
+if floor(pProgress/5) * 5 >= pProgressNext + 5
+    pProgressNext = floor(pProgress/5) * 5;
+end
+    
+if floor(pProgress) >= pProgressNext
+    captureData;
 
-        pProgressNext = pProgressNext + 5;
-        flag = 1;
-    end
-else
-    if floor(pProgress) == pProgressNext - 1
-        flag = 0;
-    end
+    pProgressNext = pProgressNext + 5;
 end
