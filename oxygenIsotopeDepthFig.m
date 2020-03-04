e1a676f975c29d2ba023eea59b041b2be4564f73
Angus Fotherby <angusfotherby@gmail.com>
2020-03-04 15:41:20 +0000

    Changed to use recycling = 14 data. Also updated depth array calculation to be general for any dz.

--- oxygenIsotopeDepthFig.m
+++ oxygenIsotopeDepthFig.m
@@ -3,7 +3,7 @@ tiledlayout(4,2);
 load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case1.mat')
 
 depth = 1:l;
-depth = depth .* sqrt(2);
+depth = depth .* dz;
 depth = depth';
 
 nexttile()
@@ -16,14 +16,14 @@ title(gca, 'Recycling = 0');
 legend('off')
 
 nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case1.mat');
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=14/case1.mat');
 labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit);
 plotOutput.depthPlot(outputd18, depth, labels);
 % Format graph.
-set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
+set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 22],'XAxisLocation','Top');
 xlabel(gca, '\delta^{18}O');
 ylabel(gca, 'Depth / cm');
-title(gca, 'Recycling = 10');
+title(gca, 'Recycling = 14');
 
 nexttile();
 load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=0/case2.mat');
@@ -36,11 +36,11 @@ ylabel(gca, {'Case 2' 'Depth / cm'})
 legend('off')
 
 nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case2.mat');
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=14/case2.mat');
 labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit)
 plotOutput.depthPlot(outputd18, depth, labels);
 % Format graph.
-set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
+set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 22],'XAxisLocation','Top');
 xlabel(gca, '\delta^{18}O');
 ylabel(gca, 'Depth / cm');
 
@@ -55,11 +55,11 @@ ylabel(gca, {'Case 3' 'Depth / cm'})
 legend('off')
 
 nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case3.mat');
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=14/case3.mat');
 labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit)
 plotOutput.depthPlot(outputd18, depth, labels);
 % Format graph.
-set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
+set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 22],'XAxisLocation','Top');
 xlabel(gca, '\delta^{18}O');
 ylabel(gca, 'Depth / cm');
 
@@ -75,10 +75,10 @@ ylabel(gca, {'Case 4' 'Depth / cm'})
 legend('off')
 
 nexttile();
-load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=10/case4.mat');
+load('/Users/angus/Dropbox/Academic/Isotope Model/Model/Case Data/pProgressRecorded/rec=14/case4.mat');
 labels = labelGen.percentageProgress(initRMSSO4, record_RMS, steadyStateLimit)
 plotOutput.depthPlot(outputd18, depth, labels);
 % Format graph.
-set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 30],'XAxisLocation','Top');
+set(gca,'YDir','reverse','YLim',[0 l * dz],'XLim',[7 22],'XAxisLocation','Top');
 xlabel(gca, '\delta^{18}O');
 ylabel(gca, 'Depth / cm');
